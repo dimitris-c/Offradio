7d31b7193ef1516f2458b8685fa2513baf22feae
dimitrisc <dimmdesign@gmail.com>
2017-04-13 22:09:30 +0100

    updated pods

---
+++ Offradio/Shared Data/OffradioWidgetShareData.swift
@@ -0,0 +1,13 @@
+//
+//  OffradioWidgetShareData.swift
+//  Offradio
+//
+//  Created by Dimitris C. on 11/04/2017.
+//  Copyright © 2017 decimal. All rights reserved.
+//
+
+import Foundation
+
+final class OffradioWidgetShareData {
+    
+}
