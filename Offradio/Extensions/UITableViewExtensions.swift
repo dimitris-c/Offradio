4b4829d60ec339915a2090dfbd4bc293da72170d
Dimitris C <dimmdesign@gmail.com>
2017-09-11 15:42:48 +0100

    Several fixes for Xcode 9 and ios 11

--- Offradio/Extensions/UITableViewExtensions.swift
+++ Offradio/Extensions/UITableViewExtensions.swift
@@ -12,11 +12,11 @@ extension UITableViewCell: Reusable { }
 
 extension UITableView {
     // Registers a UITableViewCell subclass conforming to Reusable
-    final func register<T: UITableViewCell>(cellType: T.Type) where T: Reusable {
+    final func register<T: UITableViewCell>(cellType: T.Type) {
         self.register(cellType.self, forCellReuseIdentifier: cellType.identifier)
     }
 
-    final func dequeueReusableCell<T: UITableViewCell>(`for` indexPath: IndexPath, cellType: T.Type = T.self) -> T where T: Reusable {
+    final func dequeueReusableCell<T: UITableViewCell>(`for` indexPath: IndexPath, cellType: T.Type = T.self) -> T {
         guard let cell = self.dequeueReusableCell(withIdentifier: cellType.identifier, for: indexPath) as? T else {
             fatalError("Failed to dequeue a cell with identifier \(cellType.identifier) matching type \(cellType.self). ")
         }
