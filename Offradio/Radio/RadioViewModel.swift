b03f754af40ef6eba527bcb3eb394ac528ea8825
Dimitris C <dimmdesign@gmail.com>
2017-10-16 10:51:31 +0100

    changes

--- Offradio/Radio/RadioViewModel.swift
+++ Offradio/Radio/RadioViewModel.swift
@@ -84,7 +84,7 @@ final class RadioViewModel: StormysRadioKitDelegate {
     }
 
     func srkNoNetworkFound() {
-        Log.debug("SRK: NoNetworkFound")
+        Log.debug("SRK: NoNetworkFound2")
     }
 
     func srkBadContent() {
