13f843c73030866f5e970a9bc6ca16dfe51678ee
Dimitris Chatzieleftheriou <d.chatzieleftheriou@g116413mac.thisisglobal.com>
2019-05-22 17:16:38 +0100

    Disables fetching of album artwork from LastFM

--- Offradio/Radio/OffradioMetadata.swift
+++ Offradio/Radio/OffradioMetadata.swift
@@ -14,6 +14,7 @@ import SwiftyJSON
 final class OffradioMetadata: RadioMetadata {
     fileprivate let disposeBag = DisposeBag()
     
+    private let queue = ConcurrentDispatchQueueScheduler(qos: .background)
     let nowPlaying = BehaviorRelay<NowPlaying>(value: .empty)
     
     fileprivate let crc: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
@@ -27,7 +28,7 @@ final class OffradioMetadata: RadioMetadata {
     func startTimer() {
         timerDisposeBag = DisposeBag()
         
-        let crcTimer = Observable<Int>.timer(0, period: 14, scheduler: ConcurrentDispatchQueueScheduler(qos: .background))
+        let crcTimer = Observable<Int>.timer(0, period: 14, scheduler: queue)
         let crcTimerDisposable = crcTimer.asObservable()
             .flatMapLatest({ [weak self] _ -> Observable<String> in
                 guard let strongSelf = self else { return Observable.empty() }
@@ -63,26 +64,26 @@ final class OffradioMetadata: RadioMetadata {
 
     func fetchNowPlaying() -> Observable<NowPlaying> {
         return self.nowPlayingService.rx.request(.nowPlaying)
-            .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+            .observeOn(queue)
             .mapJSON()
             .map { NowPlaying(json: JSON($0)) }
             .asObservable()
             .catchErrorJustReturn(NowPlaying.default)
-            .flatMapLatest({ (nowPlaying) -> Observable<NowPlaying> in
-                return self.fetchLastFMInfo(with: nowPlaying)
-            })
+//            .flatMapLatest({ (nowPlaying) -> Observable<NowPlaying> in
+//                return self.fetchLastFMInfo(with: nowPlaying)
+//            })
     }
 
     // Currently Not Used
     fileprivate func fetchLastFMInfo(with nowPlaying: NowPlaying) -> Observable<NowPlaying> {
         let path: LastFMAPIService = .artistInfo(artist: nowPlaying.current.artist)
         return self.lastFMApiService.rx.request(path)
-            .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+            .observeOn(queue)
             .mapJSON()
             .map { JSON($0) }
             .map { LastFMArtist(with: $0["artist"]) }
             .map { artist -> NowPlaying in
-                let filtered = artist.images.filter { $0.size == "mega" || $0.size == "large" }
+                let filtered = artist.images.filter { $0.size == "mega" || $0.size == "extralarge" }
                 if let image = filtered.first {
                     return nowPlaying.update(with: image.url)
                 }
